82c5b4c728a06f82bb090947eabd61f636033f80
Mayursinh Jadeja <mayursinhdjadeja01@gmail.com>
2025-11-15 14:37:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Function to handle visitor form submission
+
+function handleVisitorForm() {
+    alert("Welcome to Grow With Us! Your details have been submitted successfully. You're now being redirected to our homepage.");
+    return true;
+}
+
+// Function to handle contact form submission
+
+function handleContactForm() {
+    alert("Thank you for contacting us! Your message has been sent successfully.");
+    return true; 
+}
